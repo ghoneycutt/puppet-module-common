84646500952cd5923666fba3109013647f198610
Julien Kassar <github@kassisol.com>
2014-11-12 17:28:22 -0500

    Change group variable for .ssh directory

--- manifests/mkuser.pp
+++ manifests/mkuser.pp
@@ -153,7 +153,7 @@ define common::mkuser (
         ensure  => directory,
         mode    => '0700',
         owner   => $name,
-        group   => $name,
+        group   => $mygroup,
         require => User[$name],
       } # file
     } # 'ensure' or true
